5a356bf4034312f61e14487c4156adb1769a9620
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-06-09 19:50:28 -0300

    remove the img tag and add a Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import {PencilLine } from "phosphor-react"; 
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css"; 
 
@@ -11,10 +12,7 @@ export function Sidebar(){
         src="https://images.unsplash.com/photo-1654541696896-bd5d8feed3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar} 
-          src="https://github.com/Matheusmottaa.png" 
-        />
+        <Avatar hasBorder src="https://github.com/Matheusmottaa.png"/>
         <strong>John Doe</strong>
         <span>Full Stack Developer</span>
       </div>
